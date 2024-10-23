12f54f92a0e4e53bf82e68c5c7920f72d6b0e19f
Kadagana <kobiemagana@gmail.com>
2024-10-23 16:11:56 -0500

    start

--- project/src/App.js
+++ project/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/importpizzadata.php')
+        .then(response => {
+          console.log('Data received:', response.data);
+          setData(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Sample Data</h1>
+        <ul>
+          {data.map(item => (
+              <li key={item.ID}>
+                ID: {item.ID}, Sample Column: {item.samplecol}
+              </li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
